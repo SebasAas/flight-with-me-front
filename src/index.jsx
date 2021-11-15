cbc91670971f8dc7809e00caff29b0ab91539f0b
Sebastian <sebastiansoplan@outlook.com>
2021-11-15 19:33:47 -0300

    changing way to route

--- src/index.jsx
+++ src/index.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './styles/index.css'
 import App from './components/App'
-import { BrowserRouter as Router } from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom';
 
 ReactDOM.render(
   <React.StrictMode>
